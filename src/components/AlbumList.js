df075faab374e3212fb1fea3a52ada1987d094e9
rasikag <rasikagayan90@gmail.com>
2018-02-20 23:53:55 +0800

    added map

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
 
+    state = {albums :[]}
     componentWillMount(){
-        console.log('componentWillMount in AlbumList');
+        axios.get('here goes the url')
+            // this for one line 
+            //.then(response => console.log(response));
+            .then(response => {
+                this.setState({albums : response.data});
+            });
+    }
+
+    renderAlbums(){
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
     }
 
     render() {
