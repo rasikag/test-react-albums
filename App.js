284240266c14ae091201f40b525ea88ae56888a7
rasikag <rasikagayan90@gmail.com>
2018-02-22 02:20:34 +0800

    component data pass done

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
 import Header from './src/components/Header';
-import AlbumList from './src/components/AlbumList';
 
-// const App = () => {
-//   return (
-//     <View>
-//       <Header header={'My Album'} />
-//       <AlbumList />
-//     </View>
-//   );
-// };
-
-export default class App extends Component{
-
-  render (){
+class App extends Component {
+  render() {
     return (
       <View>
-        <Header header='My Album'/>
-        {/* <AlbumList /> */}
+        <Header header='My Album' />
       </View>
     )
   };
 }
+
+export default App;
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-class Header extends Component {
+class Header extends Component{
 
     render() {
         return (
